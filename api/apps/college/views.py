3aa89a1070805b4ed41899926c8bc3d604bcbe66
Osvaldo Alves <oalvesxp@gmail.com>
2024-01-05 12:29:58 -0300

    finishing function to response student value by JSON format

--- api/apps/college/views.py
+++ api/apps/college/views.py
@@ -1,4 +1,6 @@
 from django.http import JsonResponse
 
 def students(request):
-    pass
+    if request.method == 'GET':
+        student = {'id':1, 'nome':'Osvaldo'}
+        return JsonResponse(student)
