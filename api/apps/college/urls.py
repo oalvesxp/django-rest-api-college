65e6f3e01cd3f5b8c6deb7a88eb875c54f71732f
Osvaldo Alves <oalvesxp@gmail.com>
2024-01-05 15:31:12 -0300

    setuping the viewset

--- api/apps/college/urls.py
+++ api/apps/college/urls.py
@@ -1,7 +1,12 @@
-from django.urls import path
+from django.urls import path, include
 from apps.college.views import \
-    students
+    StudentsViewset, CoursesViewset
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register('alunos', StudentsViewset, basename='Alunos')
+router.register('cursos', CoursesViewset, basename='Cursos')
 
 urlpatterns = [
-    path('alunos/', students),
+    path('', include(router.urls) ),
 ]
--- api/apps/college/views.py
+++ api/apps/college/views.py
@@ -1,6 +1,15 @@
-from django.http import JsonResponse
+from rest_framework import viewsets
+from apps.college.models import \
+    Student, Course
+from apps.college.serializer import \
+    StudentSrializer, CourseSerializer
 
-def students(request):
-    if request.method == 'GET':
-        student = {'id':1, 'nome':'Osvaldo'}
-        return JsonResponse(student)
+class StudentsViewset(viewsets.ModelViewSet):
+    '''Exibindo todos os alunos e alunas'''
+    queryset = Student.objects.all()
+    serializer_class = StudentSrializer
+
+class CoursesViewset(viewsets.ModelViewSet):
+    '''Exibindo todos os cursos'''
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
