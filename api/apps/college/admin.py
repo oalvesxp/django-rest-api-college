2691d86d4daaa8c9980e141263776b4eb2173a2e
Osvaldo Alves <oalvesxp@gmail.com>
2024-01-05 13:06:18 -0300

    stuping admin with students display

--- api/apps/college/admin.py
+++ api/apps/college/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from apps.college.models import \
+    Student, Course
 
-# Register your models here.
+class Students(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'name',
+        'rg',
+        'cpf',
+        'birth',
+    )
+    list_display_links = ('id', 'name',)
+    search_fields = ('name',)
+    list_per_page = 20
+
+admin.site.register(Student, Students)
