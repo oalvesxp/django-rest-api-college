82a6ff10d579f98e6282e9d72f39d8256a67a7c1
Osvaldo Alves <oalvesxp@gmail.com>
2024-01-05 12:52:07 -0300

    creating models for students

--- api/apps/college/models.py
+++ api/apps/college/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Student(models.Model):
+    name = models.CharField(max_lenght=30)
+    rg = models.CharField(max_lenght=9)
+    cpf = models.CharField(max_lenght=11)
+    birth = models.DateField()
+
+    def __str__(self):
+        return self.name
