7eb8e65c9cfc6fa23012d66839c4ed4d43ec7a49
Osvaldo Alves <oalvesxp@gmail.com>
2024-01-07 13:54:44 -0300

    adding authentication method to consume content from api

--- api/apps/college/views.py
+++ api/apps/college/views.py
@@ -3,21 +3,29 @@ from apps.college.models import \
     Student, Course, Enrolment
 from apps.college.serializer import \
     StudentSrializer, CourseSerializer, EnrolmentSerializer, StudentEnrolmentListSerializer, CourseStudentListSerializer
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
 
 class StudentsViewset(viewsets.ModelViewSet):
     '''Exibindo todos os alunos e alunas'''
     queryset = Student.objects.all()
     serializer_class = StudentSrializer
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
 
 class CoursesViewset(viewsets.ModelViewSet):
     '''Exibindo todos os cursos'''
     queryset = Course.objects.all()
     serializer_class = CourseSerializer
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
 
 class EnrolmentViewset(viewsets.ModelViewSet):
     '''Exibindo todas as matrículas'''
     queryset = Enrolment.objects.all()
     serializer_class = EnrolmentSerializer
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
 
 class StudentEnrolmentList(generics.ListAPIView):
     '''Exibindo todas as matrículas de um aluno específico'''
@@ -25,6 +33,8 @@ class StudentEnrolmentList(generics.ListAPIView):
         queryset = Enrolment.objects.filter(student_id=self.kwargs['pk'])
         return queryset
     serializer_class = StudentEnrolmentListSerializer
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
 
 class CourseStudentList(generics.ListAPIView):
     '''Exibindo alunos matrículados em um determinado curso'''
@@ -32,3 +42,5 @@ class CourseStudentList(generics.ListAPIView):
         queryset = Enrolment.objects.filter(course_id=self.kwargs['pk'])
         return queryset
     serializer_class = CourseStudentListSerializer
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
